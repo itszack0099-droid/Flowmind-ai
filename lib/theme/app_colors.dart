00380f474b02250e5d65f69f2f608007bc56e651
itszack0099-droid <itszack0099@gmail.com>
2026-04-07 17:24:59 +0530

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mint = Color(0xFF00D4AA);
+  static const Color blue = Color(0xFF4A9FFF);
+  static const Color purple = Color(0xFF9B6BFF);
+  static const Color pink = Color(0xFFFF6B9D);
+  static const Color orange = Color(0xFFFFB74D);
+
+  static const Color background = Color(0xFF0F0F1E);
+  static const Color card = Color(0xFF1A1A2E);
+  static const Color glass = Color(0xFF25253A);
+}
